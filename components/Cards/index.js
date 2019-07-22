82ac2cd051c7d0c3c3272f4e96cd3f8081524e5c
Katrina Roaix <kroaix@gmail.com>
2019-07-21 21:54:14 -0400

    MVP Complete

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,7 +1,7 @@
 // STEP 3: Create Article cards.
 // -----------------------
 // Send an HTTP GET request to the following address: https://lambda-times-backend.herokuapp.com/articles
-// Stduy the response data you get back, closely.
+// Study the response data you get back, closely.
 // You will be creating a component for each 'article' in the list.
 // This won't be as easy as just iterating over an array though.
 // Create a function that will programmatically create the following DOM component:
@@ -17,3 +17,46 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+  .then((response) => {
+    console.log('Success!', response);
+    newArticle(response.data.articles);
+  })
+  .catch((error) => {
+    console.log('Error: ', error);
+  })
+
+function newArticle(articles) {
+  for (let topic in articles) {
+    const cardsContainer = document.querySelector('.cards-container');
+    const topics = articles[topic];
+    topics.map((article) => {
+      const card = document.createElement('div');
+      card.classList.add('card');
+
+      const headline = document.createElement('div');
+      headline.classList.add('headline');
+      headline.textContent = article.headline;
+      card.appendChild(headline);
+
+      const author = document.createElement('div');
+      author.classList.add('author');
+      card.appendChild(author);
+
+      const imgContainer = document.createElement('div');
+      imgContainer.classList.add('img-container');
+      author.appendChild(imgContainer);
+
+      const img = document.createElement('img');
+      img.src = article.authorPhoto;
+      imgContainer.appendChild(img);
+
+      const authorName = document.createElement('span');
+      authorName.textContent = `By: ${article.authorName}`;
+      author.appendChild(authorName);
+
+      cardsContainer.appendChild(card);
+    })
+  }
+}
--- components/Header/index.js
+++ components/Header/index.js
@@ -3,10 +3,32 @@
 // Using a function create the component you see below:
 //
 //  <div class="header">
-//    <span class="date">SMARCH 28, 2019</span>
+//    <span class="date">MARCH 28, 2019</span>
 //    <h1>Lambda Times</h1>
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+const Header = () => {
+  const header = document.createElement('div');
+  header.classList.add('header');
+
+  const date = document.createElement('span');
+  date.classList.add('date');
+  date.textContent = "MARCH 28, 2019";
+  header.appendChild(date);
+
+  const h1 = document.createElement('h1');
+  h1.textContent = "Lambda Times";
+  header.appendChild(h1);
+
+  const temp = document.createElement('span');
+  temp.classList.add('temp');
+  temp.textContent = "98°";
+  header.appendChild(temp);
+  
+  return header;
+}
+
+const headerContainer = document.querySelector('.header-container');
+headerContainer.appendChild(Header());
